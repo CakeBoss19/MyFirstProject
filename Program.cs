114941eb3645fccdcbcfaa3cafbd1bfd3fe532a8
Steven <93930335+CakeBoss19@users.noreply.github.com>
2022-09-19 13:15:36 -0400

    Keeping one button

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! On e more modification");
 Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
-Console.WriteLine("Hello, C# Academy! On e more modification");
+
