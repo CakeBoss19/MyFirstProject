dbfdd238a336fe366036b6514e940f456cc6ae4c
Steven Pedersen <stevenp0294@gmail.com>
2022-09-19 13:19:12 -0400

    Fixed typo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! On e more modification");
+Console.WriteLine("Hello, C# Academy! One more modification");
